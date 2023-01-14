084edc96d7abcfe749bf8dd7f01d7fafc734142f
Gizar <rahimbaev.gizar@mail.ru>
2023-01-14 16:46:05 +0500

    max из трех чисел

---
+++ Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье чиcло: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (b > max)
+max = b;
+if (c > max)
+max = c;
+
+Console.WriteLine(max);
