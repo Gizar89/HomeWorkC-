3bfdaa11a92097e6c3783988ef1f1ba60192db09
Gizar <rahimbaev.gizar@mail.ru>
2023-01-18 18:22:38 +0500

    Задача 13 Семинар№ 2

---
+++ Task6/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите любое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n<99){
+   Console.WriteLine("третьей цифры нет");
+}
+if (n>9999){
+    Console.WriteLine((n%1000)/10/10);
+}
+
+if(n>1000 && n<9999){
+    Console.WriteLine((n%100)/10);
+
+}    
+if (n>100 && n<1000){
+    
+    Console.WriteLine((n%100)%10);
+}
+    
+
+
+
+
