be4f704729bb40a51cad7ffd22400d021c9ad506
Gizar <rahimbaev.gizar@mail.ru>
2023-01-22 17:28:21 +0500

    Семинар №3, задача 21

---
+++ Task9/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("ВВедите у1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите z1: ");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите x2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("ВВедите у2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("ВВедите z2: ");
+double z2 = Convert.ToDouble(Console.Read());
+double L = Math.Round(Math.Sqrt(Math.Pow(x2-x1,2)+Math.Pow(y1-y2,2)+Math.Pow(z1-z2,2)),2);
+Console.WriteLine(L);
