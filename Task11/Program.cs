22813751b42c7758e8c71a50902aa885e54831a3
Gizar <rahimbaev.gizar@mail.ru>
2023-01-25 14:56:50 +0500

    Семинар 4, Задача 25

---
+++ Task11/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число В: ");
+int B = Convert.ToInt32(Console.ReadLine());
+int result = A;
+for(int i = 1; i<B; i++)
+{
+    result = result * A;
+}
+Console.WriteLine($"А в степени В: {result}");
+
+
+
+
