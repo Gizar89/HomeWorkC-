8094dece102a0b59aad3a5114cd157538b15d414
Gizar <rahimbaev.gizar@mail.ru>
2023-01-25 15:39:59 +0500

    Семинар 4, Задача 27

---
+++ Task12/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while(n > 0)
+{
+    int num = n % 10;
+    n=n / 10;
+    sum = sum + num;
+}
+Console.WriteLine($"Сумма чисел в числе: {sum}");
