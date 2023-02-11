0b4201a8fd6ba983acebd10b2777aff1ab6adb46
Gizar <rahimbaev.gizar@mail.ru>
2023-02-11 19:51:16 +0500

    семинар№9,Задача 68

---
+++ Task25/Program.cs
@@ -0,0 +1,16 @@
+﻿int FunctAkkerman (int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return FunctAkkerman(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return FunctAkkerman(numberM - 1, FunctAkkerman(numberM, numberN - 1));
+return FunctAkkerman(numberM, numberN);
+}
+
+Console.Clear();
+Console.WriteLine("Введите начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N:");
+int numberN = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Функция Аккермана A({numberM},{numberN}) = {FunctAkkerman(numberM, numberN)}");
